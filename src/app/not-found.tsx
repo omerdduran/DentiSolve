9a1bfeb80c4d88746b15fc1de5b2f017b87b5caf
Ömer Duran <me@omerduran.dev>
2024-10-14 02:03:44 +0200

    feat(navigation): nav change

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
             <div className="text-center p-8  rounded-xl max-w-md w-full mx-4">
                 <h2 className="text-4xl font-bold text-gray-800 mb-4">Sayfa Bulunamadı</h2>
                 <p className="text-xl text-gray-600 mb-8">İstenen kaynak bulunamadı</p>
-                <Link href="/" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+                <Link href="/protected/dashboard" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
                     Ana Sayfaya Dön
                 </Link>
             </div>
