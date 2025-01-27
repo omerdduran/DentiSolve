dbb948e9f3892165164b25434708cbb2859ad4dc
Ömer Duran <me@omerduran.dev>
2025-01-27 17:55:36 +0100

    feat(image-processing): Add Sharp for high-performance image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,14 @@ const nextConfig = {
 
     // Optimize images
     images: {
-        domains: ['localhost'],
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '',
+                pathname: '/**',
+            },
+        ],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920],
         imageSizes: [16, 32, 48, 64, 96, 128, 256],
         minimumCacheTTL: 60,
